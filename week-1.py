0e4710ea3b5dded30382092ffe201d30221bc936
AntonGashi <antongashi12345@gmail.com>
2022-02-11 16:34:10 +0000

    pretty much finished

--- week-1.py
+++ week-1.py
@@ -1,10 +1,6 @@
-from cProfile import label
-from dataclasses import replace
-from operator import le
-from re import T
 import numpy as np
 import matplotlib.pyplot as plt
-
+from numba import jit
 
 def Task1(seed,N,M):
     np.random.seed(seed)
@@ -18,7 +14,8 @@ def Task1(seed,N,M):
 
     hist1=np.histogram(uni_rand,M)
     hist2=np.histogram(random_choice,M)
-    x1,x2=0,0
+    x1=0
+    x2=0
 
     for i in hist1[0]:
         x1+=(i-E)**2/E
@@ -31,31 +28,39 @@ def Task1(seed,N,M):
     print('The Chi squared for the uniform distribution is {} and for the random choice its {}'.format(np.round(x1,3),np.round(x2,3)))
 
     shifted_array=np.roll(uni_rand,10)
+    plt.rcParams['text.usetex'] = True
     plt.scatter(uni_rand,shifted_array,marker='x')
-    plt.title('Scatter plot of Uniform random distribution $x_n$ against $x_n+10$ for {} points'.format(N))
+    plt.title('Scatter plot of Uniform random distribution $x_n$ against $x_{n+10}$ for 10000 points')
     plt.xlabel('Uniform distribution')
     plt.ylabel('Uniform distribution + 10')
-    plt.hlines(0,0,1,'r',':')
-    plt.hlines(1,0,1,'r',':')
-    plt.vlines(0,0,1,'r',':')
-    plt.vlines(1,0,1,'r',':')
+    #plt.hlines(0,0,1,'r',':')
+    #plt.hlines(1,0,1,'r',':')
+    #plt.vlines(0,0,1,'r',':')
+    #plt.vlines(1,0,1,'r',':')
     plt.show()
 
-#Task1(7,10000,100)
+#Task1(10,1000,100)
 
+@jit
 def Task2(seed,N,time_steps):
     np.random.seed(seed)
     ###Task2
 
-    left_side,right_side=np.ones(N,dtype=int),np.zeros(N,dtype=int)
-    left_sum,right_sum=np.zeros(time_steps),np.zeros(time_steps)
+    left_side=np.ones(N,dtype=int)
+    right_side=np.zeros(N,dtype=int)
+    left_sum=np.zeros(time_steps)
+    right_sum=np.zeros(time_steps)
     plot_time=np.linspace(0,time_steps,time_steps)
-
+    random_int_array=np.zeros(time_steps)
+    dndx=np.zeros(time_steps)
 
     for i in range(time_steps):
         left_sum[i]=np.sum(left_side)
         right_sum[i]=N-left_sum[i]
         rand_index=np.random.randint(0,N)
+        random_int_array[i]=rand_index
+        dndx[i]=left_sum[i]-right_sum[i]
+
         if left_side[rand_index]==1:
             right_side[rand_index]=1
             left_side[rand_index]=0
@@ -63,19 +68,28 @@ def Task2(seed,N,time_steps):
             left_side[rand_index]=1
             right_side[rand_index]=0
 
+    #plt.plot(plot_time,left_sum,label='Left Side of Box')
+    #plt.plot(plot_time,right_sum,label='Right Side of Box')
+    #plt.title('Dispersion of {} Particles in a Box'.format(N))
+    #plt.xlabel('Time Steps')
+    #plt.ylabel('Particles on the Left and Right Side of the Box')
+    #plt.hlines(N/2,0,time_steps,'r',linestyles=':',label='Half of All Particles')
+    #plt.vlines(500,0,300,color='k',linestyles=':',label='Equilibrium point')
+    #plt.legend()
 
-    plt.plot(plot_time,left_sum,label='Left Side of Box')
-    plt.plot(plot_time,right_sum,label='Right Side of Box')
-    plt.title('')
-    plt.xlabel('Time Steps')
-    plt.ylabel('Particles on the Left and Right Side of the Box')
-    plt.hlines(N/2,0,time_steps,'r',linestyles=':',label='Half of All Particles')
-    plt.ylim((80,310))
-    plt.legend()
-    plt.show()
+    #shifted_array=np.roll(random_int_array,10)
+    #correl=np.round(np.corrcoef(random_int_array,shifted_array),3)
+    #print(correl)
+    #plt.scatter(random_int_array,shifted_array,marker='x')
+    #plt.title('Scatter plot of a Random Integer distribution $x_n$ against $x_n+10$ for {} points. \n Correlation between the two arrays of random numbers with the np.corrcoef function: {}'.format(time_steps,np.diag(correl,1)))
+    #plt.xlabel('Random Integer distribution')
+    #plt.ylabel('Random Integer distribution + 10')
+    #plt.tight_layout()
+    #plt.show()
 
-#Task2(100,300,5000)
+#Task2(1,70000,int(1e6))
 
+@jit
 def Task4(seed,N,time_steps):
     np.random.seed(seed)
     ###Task4
@@ -98,6 +112,7 @@ def Task4(seed,N,time_steps):
     #plotting the data collected
     plt.plot(plot_time,left_sum,label='Left Side of Box')
     plt.plot(plot_time,right_sum,label='Right Side of Box')
+    plt.title('Dispersion of {} Particles in a Box With a Barrier Only Allowing 25% Through'.format(N))
     plt.xlabel('Time Steps')
     plt.ylabel('Particles on the Left and Right Side of the Box')
     plt.hlines(N*0.75,0,time_steps,'r',linestyles=':',label='Three Quarters of All Particles')
@@ -105,4 +120,4 @@ def Task4(seed,N,time_steps):
     plt.legend(loc='center left')
     plt.show()
 
-Task4(1,300,5000)
+Task4(1,1000,10000)
